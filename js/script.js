89e3cf6bc4db2eaf952f8e43de5ca1734053cd97
alessandrostorchi92 <storchi.alle@gmail.com>
2024-01-12 18:36:28 +0100

    Add start screen to draw()

--- js/script.js
+++ js/script.js
@@ -28,6 +28,19 @@ function draw() {
             const cell = document.createElement("div");
             cell.classList.add("cell");
 
+            const isRowEven = rowIndex % 2 === 0;
+            const isCellEven = cellIndex % 2 === 0;
+
+            if((isRowEven && isCellEven) || (!isRowEven && !isCellEven )) {
+
+                cell.classList.add("cell-dark");
+
+            }
+
+            if(cellContent === 1) {
+                cell.classList.add("bar");
+            }
+
 
             gridGame.append(cell);
         });
